64edb78671dc50bb9dbd4b3c3f9bb3ac57c315a6
niyiweb01-del <niyiweb01@gmail.com>
2026-09-24 11:20:35 +0100

    price 7.40

--- js/products-data.js
+++ js/products-data.js
@@ -206,7 +206,7 @@ const PRODUCTS = [
     ],
     category: "audits",
     categoryLabel: "Audits & fixes",
-    price: 1.4,
+    price: 7.4,
     badge: "Quick check",
     fulfillment: "instant"
   },
